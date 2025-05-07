6dcf365fa6db9440e392fe48de727cc075235134
giovi <gi.olavarria@duocuc.cl>
2025-05-07 12:07:52 -0400

    finalizado

--- src/main/java/com/example/crudrapido/controlador/controladorEstudiante.java
+++ src/main/java/com/example/crudrapido/controlador/controladorEstudiante.java
@@ -1,15 +1,23 @@
 package com.example.crudrapido.controlador;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.crudrapido.entidades.estudiante;
 import com.example.crudrapido.servicio.servicioEstudiante;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 
 @RestController
-@RequestMapping(path = api/v1/estudiantes)
+@RequestMapping(path = "api/v1/estudiantes")
 public class controladorEstudiante {
 
     @Autowired
@@ -22,12 +30,17 @@ public class controladorEstudiante {
     public List<estudiante> getAll(){
         return servicioEstudiante.listarEstudiantes();
     }
-    @PostMapping
-    public void List<estudiante> getAll(@RequestBody estudiante estudiante){
-        return servicioEstudiante.guardarOActualizar(estudiante);
+    @GetMapping("/{id_estudiante}")
+    public Optional<estudiante> getBId(@PathVariable("id_estudiante") Long id_estudiante){
+        return servicioEstudiante.listarEstudiantes(id_estudiante);
     }
-    @DeleteMapping("/{idEstudiante}")
-    public void delete(@PathVariable ("idEstudiante") Long idEstudiante{
-        servicioEstudiante.delete(id_estudiante);
+    @PostMapping
+    public void guardarActualizar(@RequestBody estudiante estudiante){
+        servicioEstudiante.guardarOActualizar(estudiante);
+    }
+    @DeleteMapping("/{id_estudiante}")
+    public void delete(@PathVariable("id_estudiante") Long id_estudiante){
+        servicioEstudiante.borrar(id_estudiante);
     }
 }
+
